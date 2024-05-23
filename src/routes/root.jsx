2da98539be4eeebe29054d3fb6e7a8eb10605ea0
Alyona <noodlenickels@gmail.com>
2024-05-23 11:14:00 +0300

    routing

---
+++ src/routes/root.jsx
@@ -0,0 +1,42 @@
+import Card from './Card';
+import user from './profile.png';
+import cart from './basket.png';
+
+function Main() {
+    function goToCart() {
+
+    }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="header-text">Здесь мог быть ваш логотип</div>
+        <div className="icons">
+          <img src={user} alt="user" />
+          <div>
+          <img onClick={goToCart} src={cart} alt="cart" />
+          <a href={`/contacts/2`}></a>
+          </div>
+        </div>
+      </header>
+      <div className="flex">
+      <div className="Container">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className="Card-container">
+        <input className="Input" placeholder='Поиск по товарам'></input>
+      </div>
+      </div>
+      <div className="flex">
+      <div className="Container">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
